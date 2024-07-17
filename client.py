20711a27f62624cf5c690dc43a1e2b7a95b07c75
Michaelsoft-Riley <ril22002@byui.edu>
2024-07-17 00:19:40 -0600

    Added basic client connection for IO

---
+++ client.py
@@ -0,0 +1,22 @@
+import socket
+import threading
+
+server = socket.socket()
+port = 12345
+
+server.connect(("10.0.0.35", port))
+
+def send():
+    while True:
+        response = input()
+        server.send(response.encode())
+
+def recieve():
+    while True:
+        print(server.recv(1024).decode())
+
+recieve_thread = threading.Thread(target=recieve)
+recieve_thread.start()
+
+send_thread = threading.Thread(target=send)
+send_thread.start()
