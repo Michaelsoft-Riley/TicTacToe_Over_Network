dfaf2243a9b995434a2a131453b8e72df4523771
Michaelsoft-Riley <ril22002@byui.edu>
2024-07-17 00:28:59 -0600

    Added basic server IO that should allow selecting coordinates from Grid class

---
+++ server.py
@@ -0,0 +1,38 @@
+import socket
+import threading
+from grid import Grid
+
+server = socket.socket()
+port = 12345
+server.bind(('', port))
+print(f"Socket bound to {port}")
+server.listen(5)
+print("socket is listening")
+
+grid = Grid()
+
+def send():
+    while True:
+        response = input()
+        client.send(response.encode())
+
+def recieve():
+    while True:
+        response = client.recv(1024).decode()
+        if response != "":    
+            response = response.split(",")
+            print(response)
+            grid.select_slot(response, 0)
+
+
+# TODO: give each new connection a thread to allow for multiple connections and simultaneous games
+while True:
+    # socket.accept() returns (a new socket object, address)
+    client, addr = server.accept()
+    print(f"Got connection from {addr}")
+
+    recieve_thread = threading.Thread(target=recieve)
+    recieve_thread.start()
+
+    send_thread = threading.Thread(target=send)
+    send_thread.start()
