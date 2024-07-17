1b318274d04a2433dbc4fc28bb829b87d8c67fcb
Michaelsoft-Riley <ril22002@byui.edu>
2024-07-17 00:30:23 -0600

    Added select_slot and is_win. Moved is_available checks to select_slot.

--- grid.py
+++ grid.py
@@ -13,8 +13,9 @@ class Grid:
             (3, 3): ""
         }
 
-        # Team points for each row. (+or-)3 points = win. (rownumber: points)
-        rowPoints = {
+        # Team points for each row. + or - 3 points is a win. (rownumber: points)
+        # Team X is negative points, and team O is positive.
+        row_points = {
             1: 0,
             2: 0,
             3: 0,
@@ -40,6 +41,16 @@ class Grid:
         rows = [row1_v, row2_v, row3_v, row4_h, row5_h, row6_h, row7_d, row8_d]
 
 
+    # Accepts an (x,y) coordinate and assigns it to the slot at (coordinate).
+    # Gives the team a point on each row that contains (coordinate).
+    def select_slot(self, coordinate, team):
+        if self.is_available(coordinate):
+            self.assign_slot(coordinate, team)
+            self.give_point(coordinate, team)
+        else:
+            print("I can't build here!")
+
+
     # Checks that a slot doesn't belong to a team yet
     def is_available(self, coordinate):
         if self.slots[coordinate] == "":
@@ -48,36 +59,42 @@ class Grid:
 
     # TODO: add check for win condition
     # If the slot coordinate is in a row, add a point for that team under the row in row_points
+    # For each row assigned a point, uses is_win to check for victory
     # Accepts coordinate(tuple) and team(0 or 1)
     # X(0) points are negative, and O(1) points are positive
     def give_point(self, coordinate, team):
         for index, row in enumerate(self.rows):
             if coordinate in row:
+                # key for the related row in row_points
+                points_key = index + 1
                 if team == 0:   
-                    self.row_points[(index -1)] -= 1
+                    self.row_points[(points_key)] -= 1
                 if team == 1:   
-                    self.row_points[(index -1)] += 1
+                    self.row_points[(points_key)] += 1
                 
-                # win condition
-                # TODO: make this its own function. Also change the output depending on who won.
-                if self.row_points[(index-1)] > 2:
-                    print("Somebody won!")
+                # Check for victory
+                self.is_win(points_key)
                 
 
-    # TODO: both methods need is_available to be ran, so move it out of here and into a run() method or something 
+    # TODO: both methods need is_available to be ran, so move it out of here and into a run() method or something instead of calling it twice.
     # Assigns slot to specified team if it is not taken. Takes an integer for team. (X=0, O=1)
     def assign_slot(self, coordinate, team):
-        if self.is_available(coordinate):
             if team == 0:
                 self.slots[coordinate] = "X"
             elif team == 1:
                 self.slots[coordinate] = "O"
-        else:
-            print("I can't build here!")
     
 
+    # TODO: Adjust victory display for UI
+    # TODO: End or reset the game
     # Checks for three in a row
-
+    def is_win(self, points_key):
+        if self.row_points[(points_key)] > 2:
+            print("Team O wins!")
+        elif self.row_points[(points_key)] < -2:
+            print("Team X wins!")
+        else:
+            print(f"Error: {self.row_points[(points_key)]} points is not enough for a victory. How did I get here?")
             
 
     # # Generate row values
