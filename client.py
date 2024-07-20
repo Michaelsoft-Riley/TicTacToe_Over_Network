ca1c2e98cb4dd75e2434b8705650a23870cfaab2
Michaelsoft-Riley <ril22002@byui.edu>
2024-07-20 01:17:31 -0600

    Added victory, loss, and draw message windows.

--- client.py
+++ client.py
@@ -1,15 +1,12 @@
 import socket
 import threading
 from tkinter import *
+from tkinter import messagebox
 
+# TODO: check for draw
 # TODO: assign server output string values to each slot.
 # TODO: Only update buttons with changed progress.
-def update_buttons(progress):
-    for i in range(0, 9):
-        if progress[i] != "|":
-            buttons[i].config(text=progress[i])
-        else:
-            buttons[i].config(text="")
+# TODO: Figure out how to split this into a client.py and ui.py file without creating a circular import
 
 server = socket.socket()
 port = 12345
@@ -23,12 +20,35 @@ def recieve():
     while True:
         response = server.recv(1024).decode()
         if "WIN" in response:
-            if "WIN X":
+            if response == "WIN X":
                 print("You win!")
-            else:
+                victory_message()
+            elif response == "WIN O":
                 print("You lose!")
+                defeat_message()
+            else:
+                print("Draw!")
+                draw_message()
         else:
             update_buttons(response)
+
+
+def update_buttons(progress):
+    for i in range(0, 9):
+        if progress[i] != "|":
+            buttons[i].config(text=progress[i])
+        else:
+            buttons[i].config(text="")
+
+def victory_message():
+        messagebox.showinfo(title="Victory!", message="You won!")
+    
+def defeat_message():
+    messagebox.showinfo(title="Defeat!", message="You lost!")
+
+def draw_message():
+    messagebox.showinfo(title="Draw!", message="It's a draw!")
+
 
 recieve_thread = threading.Thread(target=recieve)
 recieve_thread.start()
